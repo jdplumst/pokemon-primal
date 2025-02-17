ae894a75f6ccb8b2547361de1392b6bb7f552d38
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-02-17 12:47:13 -0500

    Add back netlify cache header

--- next.config.js
+++ next.config.js
@@ -6,19 +6,19 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       headers: [
-  //         {
-  //           key: "Netlify-CDN-Cache-Control",
-  //           value: "public, max-age=0, must-revalidate",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Netlify-CDN-Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;
